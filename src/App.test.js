141818e2bb006a2cb71b102490ab97b82f99cf88
Tony Cantone <tony.cantone@tessella.com>
2023-04-06 11:33:03 +0100

    Removed frontend testing stuff

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders header title', () => {
-  render(<App />);
-  const title = screen.getByText(/To Do Manager/i);
-  expect(title).toBeInTheDocument();
-});
